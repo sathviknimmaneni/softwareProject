5234da9715f72ed6bfb6e1e4d95d5ebf050e0d8a
sathviknimmaneni <sathvik.nimmaneni@gmail.com>
2020-05-20 16:54:14 +0530

    db chnages

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //mongoose connnection and schemas
-//mongoose.connect("mongodb+srv://Sathvik:"+process.env.DBKEY+"@cluster0-deldk.mongodb.net/auctionDB",{useNewUrlParser:true, useUnifiedTopology: true});
-mongoose.connect("mongodb://localhost:27017/auctionDB",{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://Sathvik:"+process.env.DBKEY+"@cluster0-deldk.mongodb.net/auctionDB",{useNewUrlParser:true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/auctionDB",{useNewUrlParser:true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex",true);
 
 app.use(bodyParser.urlencoded({extended: true}));
