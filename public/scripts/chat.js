715d233a9be221f1988f32025bbee569a8bdc6f1
sathviknimmaneni <sathvik.nimmaneni@gmail.com>
2020-05-22 21:42:29 +0530

    man wtf

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('https://tranquil-sands-34438.herokuapp.com/chatroom');
+var socket = io.connect('https://tranquil-sands-34438.herokuapp.com/');
 //var socket = io.connect('http://localhost:3000/');
 
 // Query DOM
